06fecc21be1d489ab33e42e9f565e3c0b6d00e66
Mac Mini <macmini@macs-mac-mini.local>
2017-11-13 21:26:55 -0600

    agregando tabla y celda

---
+++ otraTabla/Cells/cellMainTable.h
@@ -0,0 +1,15 @@
+//
+//  cellMainTable.h
+//  otraTabla
+//
+//  Created by Mac Mini on 11/13/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMainTable : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgUser;
+@property (strong, nonatomic) IBOutlet UILabel *lblModelo;
+
+@end

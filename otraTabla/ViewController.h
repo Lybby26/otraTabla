8bceff098b74b4bba958b64c796939ab3fb6f687
Mac Mini <macmini@macs-mac-mini.local>
2017-11-13 20:29:05 -0600

    Initial Commit

---
+++ otraTabla/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  otraTabla
+//
+//  Created by Mac Mini on 11/13/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

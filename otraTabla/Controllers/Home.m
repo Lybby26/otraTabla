875ac85f1712ebc336188d86893da6aa87a25e52
Mac Mini <macmini@macs-mac-mini.local>
2017-11-14 21:13:52 -0600

    table created

--- otraTabla/Controllers/Home.m
+++ otraTabla/Controllers/Home.m
@@ -7,9 +7,10 @@
 //
 
 #import "Home.h"
-
+#import "cellMainTable.h"
 @interface Home ()
-
+@property NSMutableArray *imgZapato;
+@property NSMutableArray *modZapato;
 @end
 
 @implementation Home
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initController];
 }
 
 
@@ -25,5 +27,46 @@
     // Dispose of any resources that can be recreated.
 }
 
+//-------------------------------------------------------------------------------
+- (void)initController {
+    self.imgZapato  = [[NSMutableArray alloc] initWithObjects: @"zapAzul.jpg", @"zapblanco.jpg", @"zapdorado.jpg", @"zapExcentrico.jpg", @"zapNegro.jpg", @"zapRojo.jpg",@"zapRosa.jpg",@"ZapRosaBlanco.jpg",nil];
+    
+    self.modZapato  = [[NSMutableArray alloc] initWithObjects: @"Azu001", @"Bla001",@"Dor001",@"Exc001",@"Neg001",@"Roj001",@"Ros001",@"RoB001", nil];
+    
 
+}
+
+/**********************************************************************************************/
+#pragma mark - Table source and delegate methods
+/**********************************************************************************************/
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+//-------------------------------------------------------------------------------
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.modZapato.count;
+}
+//-------------------------------------------------------------------------------
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 64;
+}
+//-------------------------------------------------------------------------------
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Initialize cells
+    cellMainTable *cell = (cellMainTable *)[tableView dequeueReusableCellWithIdentifier:@"cellMainTable"];
+    
+    if (cell == nil) {
+        [tableView registerNib:[UINib nibWithNibName:@"cellMainTable" bundle:nil] forCellReuseIdentifier:@"cellMainTable"];
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cellMainTable"];
+    }
+    //Fill cell with info from arrays
+    cell.lblModelo.text       = self.modZapato[indexPath.row];
+    cell.imgUser.image      = [UIImage imageNamed:self.imgZapato[indexPath.row]];
+    
+    return cell;
+}
+//-------------------------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    //Pending
+}
 @end
